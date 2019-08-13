ba5788c4b8140947bbf5fafcaafc159a6502b9dc
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-08-13 14:58:24 -0700

    app can now establish a websocket connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath))
 app.use(express.json());
 
-module.exports = app;
+module.exports = server;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const app = require('./app');
+const server = require('./app');
 const PORT = process.env.PORT;
 const chalk = require('chalk');
+const socketio = require('socket.io');
+const io = socketio(server);
 
-app.listen(PORT, ()=> {
+io.on('connection', () => {
+  console.log(chalk.blue.bold(`New WebSocket connection`))
+});
+
+server.listen(PORT, ()=> {
   console.log(chalk.green.bold(`Server is running on port ${PORT}`))
 });
