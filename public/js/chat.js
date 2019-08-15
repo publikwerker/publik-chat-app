c71df41c6f35273d1b7962191903d21c2f0aa7a3
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-08-15 12:53:59 -0700

    buttons disable while action fetches and reenable in the callback, setting the focus to the chatFormInput

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,15 @@
 const socket = io();
-const chatForm = document.querySelector('#message-form');
+
+// ELEMENTS
+const $chatForm = document.querySelector('#message-form');
+const $chatFormInput = $chatForm.querySelector('#message');
+const $chatFormButton = $chatForm.querySelector('#send') ;
+const $locateButton = document.querySelector('#send-location');
 
 socket.on('joined', (message) => {
   console.log(message);
 });
-
+ 
 socket.on('message', (message) => {
   console.log(message);
 })
@@ -17,10 +22,18 @@ socket.on('newMessage', (message) => {
   console.log(message);
 });
 
-chatForm.addEventListener('submit', (e) => {
+$chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  // disable button until fetch is complete
+  $chatFormButton.setAttribute('disabled', 'disabled');
+  // clear message field, refocus
   const message = e.target.elements.message.value;
   socket.emit('newMessage', message, (error) => {
+    // reenable button -- fetch is complete
+    $chatFormButton.removeAttribute('disabled');
+    $chatFormInput.value = '';
+    $chatFormInput.focus();
     if (error) {
       return console.log(error)
     }
@@ -28,19 +41,24 @@ chatForm.addEventListener('submit', (e) => {
   })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
-   if (!navigator.geolocation) {
-     return alert('Geolocation is not supported by your browser');
-   }
-
-   navigator.geolocation.getCurrentPosition((position) => {
-     socket.emit('sendLocation', {
-       lat: position.coords.latitude, 
-       long: position.coords.longitude
+$locateButton
+  .addEventListener('click', () => {
+    if (!navigator.geolocation) {
+      return alert('Geolocation is not supported by your browser');
+    }
+    // disable button until fetch is complete
+    $locateButton.setAttribute('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition((position) => {
+      socket.emit('sendLocation', {
+        lat: position.coords.latitude, 
+        long: position.coords.longitude
       }, (error) => {
         if(error){
           return console.log(error)
         }
+        // reenable button -- fetch is complete
+        $locateButton.removeAttribute('disabled');
+        $chatFormInput.focus();
         console.log(`Location shared.`)
       });
    });
